57b00cfa695e8748eef5b63c6237770431cc8beb
jnytnai0613 <surf.0530.junya@gmail.com>
2025-09-19 22:17:01 +0900

    feat: 🎸 Deploy用GitHub ActionsのIAM追加

--- systems/iam/role/oidc/github/resource.tf
+++ systems/iam/role/oidc/github/resource.tf
@@ -2,6 +2,21 @@ data "aws_ecr_repository" "ecr" {
   name = "${var.service_name}-${var.env}-${var.role}"
 }
 
+data "aws_ecs_cluster" "ecs_cluster" {
+  cluster_name = "${var.service_name}-${var.env}-cluster"
+}
+
+data "aws_ecs_service" "ecs_service" {
+  cluster_arn  = data.aws_ecs_cluster.ecs_cluster.id
+  service_name = "${var.service_name}-${var.env}-service"
+}
+
+data "aws_ecs_task_definition" "task_definition" {
+  task_definition = "${var.service_name}-${var.env}-task"
+}
+
+data "aws_caller_identity" "caller_identity" {}
+
 data "aws_iam_policy_document" "ecr_push_policy" {
   statement {
     effect = "Allow"
@@ -17,7 +32,9 @@ data "aws_iam_policy_document" "ecr_push_policy" {
       "ecr:BatchGetImage"
     ]
 
-    resources = [data.aws_ecr_repository.ecr.arn]
+    resources = [
+      data.aws_ecr_repository.ecr.arn
+    ]
   }
 }
 
@@ -30,7 +47,94 @@ data "aws_iam_policy_document" "ecr_auth_policy" {
       "ecr:GetAuthorizationToken"
     ]
 
-    resources = ["*"]
+    resources = [
+      "*"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "ecr_pull_policy" {
+  statement {
+    effect = "Allow"
+    sid    = "ECRPull"
+
+    actions = [
+      "ecr:BatchGetImage",
+      "ecr:GetDownloadUrlForLayer"
+    ]
+
+    resources = [
+      data.aws_ecr_repository.ecr.arn
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "deploy_service" {
+  statement {
+    effect = "Allow"
+    sid    = "RegisterTaskDefinition"
+
+    actions = [
+      "ecs:RegisterTaskDefinition",
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    sid    = "DescribeTaskDefinition"
+
+    actions = [
+      "ecs:DescribeTaskDefinition",
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    sid    = "DeployService"
+
+    actions = [
+      "ecs:UpdateService",
+      "ecs:DescribeServices",
+    ]
+
+    resources = [
+      data.aws_ecs_service.ecs_service.arn
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    sid    = "PassRoleInTaskExecution"
+
+    actions = [
+      "iam:PassRole"
+    ]
+
+    resources = [
+      data.aws_ecs_task_definition.task_definition.execution_role_arn,
+      data.aws_ecs_task_definition.task_definition.task_role_arn
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    sid    = "TagResourceOnTaskDefinition"
+
+    actions = [
+      "ecs:TagResource"
+    ]
+
+    resources = [
+      "${data.aws_ecs_task_definition.task_definition.arn_without_revision}:*"
+    ]
   }
 }
 
@@ -45,6 +149,8 @@ module "aws_role_oidc_provider" {
   inline_policy_documents = {
     ECRAuth            = data.aws_iam_policy_document.ecr_auth_policy.json
     PushContainerImage = data.aws_iam_policy_document.ecr_push_policy.json
+    PullContainerImage = data.aws_iam_policy_document.ecr_pull_policy.json
+    DeployService      = data.aws_iam_policy_document.deploy_service.json
   }
 
   iam_role_additional_tags = {
